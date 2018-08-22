3b19eac4ca7e449766f6895162b45fe2e3db888b
Zheng Cao <zheng.cao@milliman.com>
2018-08-22 14:33:40 -0700

    Convert coal dataset from long to wide

--- dataWrangling.R
+++ dataWrangling.R
@@ -8,3 +8,12 @@ glimpse(coal)
 
 colnames(coal)[1] <- 'region'
 summary(coal)
+
+coal_long <- gather(coal, 'year', 'coal_consumption', -region)
+glimpse(coal_long)
+
+coal_long$year <- as.integer(coal_long$year)
+summary(coal_long)
+
+coal_long$coal_consumption <- as.numeric(coal_long$coal_consumption)
+summary(coal_long)
